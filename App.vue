bf2182e50cb41af2677f2a013ebd4506c28b565a
Daniel Wagner <rocklabmusiker@gmail.com>
2021-01-05 18:09:23 +0100

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,37 @@
+<template>
+  <AppHeader @open-login-modal="isLoginOpen = true" />
+  <div class="w-full flex">
+    <router-view></router-view>
+  </div>
+  <LoginModal v-if="isLoginOpen" @close-login-modal="isLoginOpen = false"/>
+</template>
+
+<script>
+import AppHeader from "./components/AppHeader";
+import LoginModal from "./components/LoginModal";
+import firebase from "./utilities/firebase";
+export default {
+  data() {
+    return {
+      isLoginOpen: false,
+      isLoggedIn: false,
+      authUser: {},
+    };
+  },
+  mounted() {
+    firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    this.isLoggedIn = true;
+    this.authUser = user;
+  } else {
+    this.isLoggedIn = false;
+    this.authUser = {};
+  }
+});
+  },
+  components: {
+    AppHeader,
+    LoginModal,
+  },
+};
+</script>
